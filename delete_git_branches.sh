a369b1578d52bafd71184c2b3e04318660d9f01a
Jonathan Rwabahizi <jonathanrwabahizi@gmail.com>
2022-01-31 00:26:49 +0200

    Update delete_git_branches.sh

--- delete_git_branches.sh
+++ delete_git_branches.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
-command="git branch"
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --branches=*)
+    branches="${1#*=}"
+    ;;
+  --b=*) # alias for --branches
+    b="${1#*=}"
+    ;;
+  --exclude=*)
+    exclude="${1#*=}"
+    ;;
+  --e=*) # alias for --exclude
+    e="${1#*=}"
+    ;;
+  --force=*)
+    force="${1#*=}"
+    ;;
+  --f=*) # alias for --force
+    f="${1#*=}"
+    ;;
+  *)
+    printf "***************************\n"
+    printf "* Error: Invalid argument (${1}).\n"
+    printf "***************************\n"
+    exit 1
+    ;;
+  esac
+  shift
+done
 
-if [[ $@ == "" ]]; then
-  echo "Branches to delete are required! Example: delete_git_branches branch-1 branch-2 ... branch-n"
+branches=$(echo ${b:-$branches} | sed -e 's/[][]//g;s/ //g;s/,/ /g')
+exclude=$(echo ${e:-$exclude} | sed -e 's/[][]//g;s/ //g;s/,/ /g')
+force=$(echo ${f:-$force} | tr '[:upper:]' '[:lower:]')
+
+if [[ $branches == "" ]] && [[ $exclude == "" ]]; then
+  echo "Branches to delete or to exclude are required!"
+  echo "Example \"delete_git_branches --branches=br-1,br-2...br-n\" or \"delete_git_branches --exclude=br-1,br-2...br-n\""
   exit 1
 fi
 
-for branch in $@; do
-  command="$command | grep -v \"$branch\""
-done
+if [[ $branches ]] && [[ $exclude == "" ]]; then
+  echo "Do you want to delete all branches(${branches// /,})[y=Yes, n=No]?: "
+else
+  echo "Do you want to delete all branches except(${exclude// /,})[y=Yes, n=No]?: "
+fi
 
-command="$command | xargs git branch -D"
+read answer
+answer=$(echo $answer | tr '[:upper:]' '[:lower:]')
+
+if [[ $answer == "no" ]] || [[ $answer == "n" ]]; then
+  exit 1
+fi
+
+if [[ $force == "yes" ]] || [[ $force == "y" ]]; then
+  if [[ $branches ]]; then
+    command="git branch -D $branches"
+  fi
+  if [[ $exclude ]] && [[ $branches == "" ]]; then
+    command="git branch | grep -v ${exclude// / | grep -v } | grep -v $(git br --show-current) | xargs git branch -D"
+  fi
+else
+  if [[ $branches ]]; then
+    command="git branch -d $branches"
+  fi
+  if [[ $exclude ]] && [[ $branches == "" ]]; then
+    command="git branch | grep -v ${exclude// / | grep -v } | grep -v $(git br --show-current) | xargs git branch -d"
+  fi
+fi
 
 echo $command
+eval $command
